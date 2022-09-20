c7458b78dbe539619ca91dfe9d9d874500fafbdd
IreneCreadora <nargiza.dancer@gmail.com>
2022-09-20 15:41:48 +0300

    create primary task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,33 @@
-// Add imports above this line
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 import { galleryItems } from './gallery-items';
-// Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  galleryContainer: document.querySelector('.gallery'),
+};
+const galleryMarkup = createGalleryCardsMarkup(galleryItems);
+refs.galleryContainer.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryCardsMarkup(array) {
+  return array
+    .map(({ preview, original, description }) => {
+      return `
+    <div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+        `;
+    })
+    .join('');
+}
+const lightBox = new SimpleLightbox('.gallery a', {
+  captions: true,
+  captionsData: 'alt',
+  captionDelay: 250,
+});
