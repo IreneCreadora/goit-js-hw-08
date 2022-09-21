f35d34499b3b6a380c668ee226146480f61a57d6
IreneCreadora <nargiza.dancer@gmail.com>
2022-09-21 17:36:44 +0300

    create third task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,35 @@
+import throttle from 'lodash.throttle';
 
+const FORM_KEY = 'feedback-form-state';
+const formData = {};
+
+const refs = {
+  form: document.querySelector('.feedback-form'),
+};
+
+refs.form.addEventListener('submit', onFormSubmit);
+refs.form.addEventListener('input', throttle(onFormInput, 500));
+
+function onFormInput(e) {
+  const key = e.target.name;
+  const value = e.target.value;
+  formData[key] = value;
+  localStorage.setItem(FORM_KEY, JSON.stringify(formData));
+}
+
+updateForm();
+
+function updateForm() {
+  if (localStorage.getItem(FORM_KEY)) {
+    const { email, message } = JSON.parse(localStorage.getItem(FORM_KEY));
+    refs.form.email.value = email;
+    refs.form.message.value = message;
+  }
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+  console.log(JSON.parse(localStorage.getItem(FORM_KEY)));
+  localStorage.removeItem(FORM_KEY);
+  e.target.reset();
+}
